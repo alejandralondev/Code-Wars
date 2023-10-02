8c9dbe6021e88d7ebcf393d94dbfcf1aa1974f00
Alejandra Londono <alejandralondono@Alejandras-MacBook-Air.local>
2023-10-02 17:18:12 -0400

    adding 10-2-2023

---
+++ 10-2-2023.js
@@ -0,0 +1,9 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+    return (number * -1)
+  }
+  //return is used to return a particular value from the function. 
+  // The function will stop the execution when the return statement is called and return a specific value. 
+  // The return statement should be the last statement of the function because the code after the return statement won't be accessible
+  // by multiplying the number by a negative one we get the opposite, the numbers negative value
