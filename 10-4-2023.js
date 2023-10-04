1791091042e30dc2708ef0abf6cfc5e7f1f8add6
Alejandra Londono <alejandralondono@Alejandras-MacBook-Air.local>
2023-10-04 19:43:41 -0400

    first commit

---
+++ 10-4-2023.js
@@ -0,0 +1,19 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+function abbrevName(name){
+    //first split the two words into first and last name
+    //make variable for the first and last name
+    //use split method to split a string into an array of substrings
+      let [firstName, lastName] = name.split(" ");
+      //target first name array, create variable for first initical and capitalize it using to upper case
+      let firstInitial = firstName[0].toUpperCase();
+      //do it for the last name
+      let lastInitial = lastName[0].toUpperCase();
+      
+      //return the initials separated by a dot
+      return `${firstInitial}.${lastInitial}`;
+  }
