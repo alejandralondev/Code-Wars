45b36b4b86139eb42fa08176a4e6fdfb9d866d13
alejandralondev <aledonolon@outlook.com>
2023-11-21 14:13:07 -0500

    first commit

---
+++ 11-21-2023.js
@@ -0,0 +1,10 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool) {
+    // Check if the boolean value is true or false
+    if (bool === true) {
+      return "Yes";
+    } else {
+      return "No";
+    }
+  }
