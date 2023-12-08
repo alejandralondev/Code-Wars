d7863f977442bd9998ddb2b6a92b175ecf6d4a14
alejandralondev <aledonolon@outlook.com>
2023-12-07 23:39:29 -0500

    first commit

---
+++ 12-7-2023.js
@@ -0,0 +1,12 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+function solution(str){
+    // Split the string into an array of characters
+    // reverse the array
+    // join it back to  a string
+    return str.split('').reverse().join('');
+  }
