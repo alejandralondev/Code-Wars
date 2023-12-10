f0aba50b07e78f5b66095d6913164f6a780fc74a
alejandralondev <aledonolon@outlook.com>
2023-12-09 22:44:21 -0500

    first commit

---
+++ 12-9-2023.js
@@ -0,0 +1,20 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+    let sum = 0;
+  
+    // Loop through the array
+    for (let i = 0; i < arr.length; i++) {
+      // Check if the number is positive
+      if (arr[i] > 0) {
+        // Add positive numbers to the sum
+        sum += arr[i];
+      }
+    }
+  
+    return sum;
+  }
